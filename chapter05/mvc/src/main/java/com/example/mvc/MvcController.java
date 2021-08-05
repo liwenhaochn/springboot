b50c00239dc453b5828b01781bf384c4e920d1b5
liwenhao <liwenhaochn@gmail.com>
2021-08-05 08:44:26 +0800

    修改mvc模块

--- chapter05/mvc/src/main/java/com/example/mvc/MvcController.java
+++ chapter05/mvc/src/main/java/com/example/mvc/MvcController.java
@@ -1,17 +1,17 @@
 package com.example.mvc;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MvcController {
 
     @RequestMapping("/hello")
     public String hello(Model model) throws Exception {
-        model.addAttribute("mvc","Hello, SpringBoot!");
+        model.addAttribute("var","hello,Spring MVC!");
         return "view";
     }
-
 }
