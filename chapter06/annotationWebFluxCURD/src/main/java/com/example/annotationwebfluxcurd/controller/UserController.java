1cd0b7b41db301c885b46bcf51bf1262a131d09a
liwenhao <liwenhaochn@gmail.com>
2021-08-09 16:28:07 +0800

    完善AnnotationWebFluxCURD模块，并使用PostMan测试

--- chapter06/annotationWebFluxCURD/src/main/java/com/example/annotationwebfluxcurd/controller/UserController.java
+++ chapter06/annotationWebFluxCURD/src/main/java/com/example/annotationwebfluxcurd/controller/UserController.java
@@ -1,11 +1,11 @@
 package com.example.annotationwebfluxcurd.controller;
 
 import com.example.annotationwebfluxcurd.entity.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import javax.annotation.PostConstruct;
 import java.util.HashMap;
@@ -13,6 +13,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("/annotationWebFluxCURD")
 public class UserController {
     Map<Long, User> users = new HashMap<>();
 
@@ -26,9 +27,45 @@ public class UserController {
         users.put(Long.valueOf(2), new User(990002, "chenyijun", 26));
     }
 
-    @GetMapping("/annotationWebFluxCURD")
+    @GetMapping("/getall")
     public Flux<User> getAll(){
         return Flux.fromIterable(users.entrySet().stream().map(entity -> entity.getValue()).collect(Collectors.toList()));
     }
+
+    /*
+    * 这里的id是users的第1个Long类型的元素（1或2）
+    * */
+    @GetMapping("/{id}")
+    public Mono<User> getUser(@PathVariable Long id){
+        return Mono.justOrEmpty(users.get(id));
+    }
+
+    /*
+    * 添加用户
+    * */
+    @PostMapping("")
+    public Mono<ResponseEntity<String>> addUser(User user){
+        users.put(user.getId(),user);
+        return Mono.just(new ResponseEntity<>("添加成功", HttpStatus.CREATED));
+    }
+    /*
+    * 修改用户
+    * */
+    @PutMapping("/{id}")
+    public Mono<ResponseEntity<User>> amendUser(@PathVariable Long id, User user){
+        user.setId(id);
+        users.put(id,user);
+        return Mono.just(new ResponseEntity<>(user, HttpStatus.CREATED));
+    }
+
+    /*
+    * 删除用户
+    * */
+    @DeleteMapping("/{id}")
+    public Mono<ResponseEntity<String>> deleteUser(@PathVariable Long id){
+        users.remove(id);
+        return Mono.just(new ResponseEntity<>("删除成功", HttpStatus.ACCEPTED));
+    }
+
 
 }
