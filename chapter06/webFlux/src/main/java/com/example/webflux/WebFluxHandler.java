bac0a22902790937a5fa1a93346a3387e8c405a4
liwenhao <liwenhaochn@gmail.com>
2021-08-09 18:06:14 +0800

    添加webFlux模块

---
+++ chapter06/webFlux/src/main/java/com/example/webflux/WebFluxHandler.java
@@ -0,0 +1,15 @@
+package com.example.webflux;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Component
+public class WebFluxHandler {
+    public Mono<ServerResponse> hello(ServerRequest serverRequest) {
+        return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN)
+                .body(Mono.just("hello,WebFlux!!!"), String.class);
+    }
+}
