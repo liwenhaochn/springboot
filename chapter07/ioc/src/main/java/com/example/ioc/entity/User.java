77368bed4bd8faa3a1fad071d2f7710abda02ad1
liwenhao <liwenhaochn@gmail.com>
2021-11-11 13:04:11 +0800

    修改ioc模块

--- chapter07/ioc/src/main/java/com/example/ioc/entity/User.java
+++ chapter07/ioc/src/main/java/com/example/ioc/entity/User.java
@@ -1,11 +1,34 @@
 package com.example.ioc.entity;
 
-import lombok.Data;
 
 import java.io.Serializable;
 
-@Data
+
 public class User implements Serializable {
     private int id;
     private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
