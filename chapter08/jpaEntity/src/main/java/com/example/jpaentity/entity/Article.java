43d563ff77c31bb42e031f6665944eb47dc5619f
liwenhao <liwenhaochn@gmail.com>
2021-11-13 19:55:23 +0800

    修改jpaEntity模块

--- chapter08/jpaEntity/src/main/java/com/example/jpaentity/entity/Article.java
+++ chapter08/jpaEntity/src/main/java/com/example/jpaentity/entity/Article.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.persistence.Transient;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
@@ -16,6 +17,7 @@ import java.util.List;
 
 @Entity
 @Data
+@Table(name = "jpaEntity")
 public class Article implements Serializable {
 
     @Id
